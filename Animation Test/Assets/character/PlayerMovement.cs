90b44c8fb403837ed209faa1026ae175756d1ca6
Jackson <jackson.lanaus@gmail.com>
2020-02-08 06:10:01 -0500

    Player animations, and some much shit fixed

--- Animation Test/Assets/character/PlayerMovement.cs
+++ Animation Test/Assets/character/PlayerMovement.cs
@@ -5,39 +5,129 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public Camera cam;
+    
     public Animator animator;
     public float mH;
     public float mV;
     public float speed;
+    public float gravityForce;
+    public bool IsOnGround;
+
+
+
+    private Transform cam;
     private Rigidbody rb;
+    private Vector3 lookPos;
+    private Vector3 camForward;
+    private Vector3 move;
+    private Vector3 moveInput;
+    private float forwardAmount;
+    private float turnAmount;
+    private float gravity;
+
+    public float JumpHeight;
+    private bool IsJumping;
+
     
-    float angle;
 
     void Start()
     {
+
         //Animator animator = GetComponent<Animator>();
-        
+        IsJumping = false;
+        IsOnGround = false;
+        gravityForce = gravityForce / -1;
         rb = GetComponent<Rigidbody>();
-        
+        cam = Camera.main.transform;
     }
-    private void Update()
-    {
-        Rotate();
-        Animate();
-    }
+
     void FixedUpdate()
     {
         mH = Input.GetAxis("Horizontal");
-        mV = Input.GetAxis("Vertical");
-        rb.velocity = new Vector3(mH * speed, rb.velocity.y, mV * speed);  
+        mV = Input.GetAxis("Vertical") ;
+
+        groundCheck();
+
+        if (IsOnGround == true)
+        {
+            gravity = 0;
+            
+        }
+
+        if (IsOnGround == false)
+        {
+            gravity = gravityForce;
+        }
+
+    
+        
+        // setting the moemevnts for animations to be reletive to the camera
+        if(cam != null)
+        {
+            camForward = Vector3.Scale(cam.up, new Vector3(1,0,1).normalized);
+            move = mV * camForward + mH * cam.right;
+        }
+        else
+        {
+            move = mV * transform.forward + mH * transform.right;
+        }
+        if(move.magnitude > 1)
+        {
+            move.Normalize();
+        }
+        // rotate towards the mouse
+        Rotate();
+        // Check for ground (Main for animations, but also for jump limits)
+        
+        
+        Move(move);
+
+        Vector3 movement = new Vector3(mH, gravity, mV * 2.2f);
+        rb.velocity = movement * speed;
+
     }
-    void Animate()
+
+    // so far not working
+    void JumpPressed()
+    {
+        if (Input.GetButtonDown("Jump") && IsOnGround == true)
+        {
+            Debug.Log("Jumped");
+            rb.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+            
+            
+        }
+    }
+
+    void Move(Vector3 move)
+    {
+        if(move.magnitude > 1)
+        {
+            move.Normalize();
+        }
+
+        this.moveInput = move;
+
+        ConverMoveInput();
+        UpdateAnimator();
+
+        
+    }
+
+    void ConverMoveInput()
+    {
+        Vector3 localMove = transform.InverseTransformDirection(moveInput);
+        turnAmount = localMove.x;
+        forwardAmount = localMove.z;
+    }
+
+    
+    void UpdateAnimator()
     {
         /* START LOCOMOTION */
         // Update the Animator with our values so that the blend tree updates
-        animator.SetFloat("VelX", mH/2);
-        animator.SetFloat("VelZ", mV/2);
+        animator.SetFloat("Forward", forwardAmount * 5);
+        animator.SetFloat("Turn", turnAmount);
         /* END LOCOMOTION */
 
     }
@@ -49,12 +139,33 @@ public class PlayerMovement : MonoBehaviour
         
         if (Physics.Raycast(_ray ,out _hit))
         {
-            transform.LookAt(new Vector3(_hit.point.x, transform.position.y, _hit.point.z));
-
-            
+            lookPos = _hit.point;
         }
+
+        Vector3 lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+
+        transform.LookAt(transform.position + lookDir, Vector3.up);
 
     }
 
+    void groundCheck()
+    {
+        RaycastHit _gHit;
+        Ray _gRay = new Ray(transform.position, -transform.up);
+
+        if (Physics.Raycast(_gRay, out _gHit, 1.5f))
+        {
+            IsOnGround = true;
+        }
+        else
+        {
+            IsOnGround = false;
+        }
+
+        Debug.DrawRay(transform.position, -transform.up * 1.6f, Color.white);
+    }
+
+    
     
 }
